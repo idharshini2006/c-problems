171ddca39d3370d9a43e9f5b81f805e3bf0fbb21
Dharshini.I <idharshini198@gmail.com>
2026-02-12 19:46:59 +0530

    Add files via upload

---
+++ nested for prob 4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+    for (int i=1;i<=5;i++) {
+        for (int j=1;j<i+1;j++) {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}//
+// Created by DELL on 12-02-2026.
+//
